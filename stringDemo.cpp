d8733db98914aaa7baf294aa20882e7b3f8b07fb
wangyihan0618 <67740197+wangyihan0618@users.noreply.github.com>
2022-10-10 19:34:45 +0800

    Update stringDemo.cpp

--- stringDemo.cpp
+++ stringDemo.cpp
@@ -5,10 +5,10 @@ int main() {
     string s("helloworld!!!");
     string ans = "";
     for (int i = 0; i < s.size(); i++) {
-        if (ispunct(s[i])) { //判断s[i]是不是符号，如果是符号ispusnct返回true，执行continue，不会添加到ans，如果不是符号，添加到ans
-            continue;
+        if (!ispunct(s[i])) { //判断s[i]是不是符号，如果不是符号，加到ans中
+            ans += s[i];
         }
-        ans += s[i];
+        
     }
     cout << ans; //输出ans即为没有符号的字符串
     return 0;
